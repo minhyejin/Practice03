7028908c6ed5911af17e4eed798c1f97847aea81
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-09 19:32:34 +0900

    8st

--- src/com/javaex/problem08/Book.java
+++ src/com/javaex/problem08/Book.java
@@ -1,7 +1,51 @@
 package com.javaex.problem08;
 
 public class Book {
-    
+    int bookNo;
+    String title;
+    String author;
+    int stateCode;
+	
+    public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author=author;
+		this.stateCode =1;
+	}
+    public int getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getStateCode() {
+		return stateCode;
+	}
+	
+	public void rent() {
+    	System.out.println(title+"대여 되었습니다.");
+    }
+    public void print() {
+    	if(stateCode==0) {
+    		System.out.println("대여중");
+    	}else if(stateCode==1) {
+    		System.out.println("재고있음");
+    	}
+    	
+    	
+    }
     
     
     
--- src/com/javaex/problem08/BookShop.java
+++ src/com/javaex/problem08/BookShop.java
@@ -25,10 +25,10 @@ public class BookShop {
         System.out.print("대여 하고 싶은 책의 번호를 입력하세요:");
         int num = scanner.nextInt();
         scanner.close();
-
+        
         // (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
        
-
+       books[num].rent();
         System.out.println("*****도서 정보 출력하기******");
         displayBookInfo(books);
     }
@@ -36,5 +36,8 @@ public class BookShop {
     //(2)전달받은 배열을 모두 출력하는 메소드
     private static void displayBookInfo(Book[] books) {
         //코드작성
+    	for(int i=0; i<books.length; i++) {
+    		System.out.println(books);
+    	}
     }
 }
