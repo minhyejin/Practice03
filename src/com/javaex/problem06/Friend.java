c59409c9bf54036fb740b85c206e53da7e178935
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-09 19:39:53 +0900

    6

--- src/com/javaex/problem06/Friend.java
+++ src/com/javaex/problem06/Friend.java
@@ -6,11 +6,7 @@ public class Friend {
     private String hp;
     private String school;
 
-    
-    //필요한 메소드 작성
-    
-    
-    
+
     public void showInfo(){
         System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
     }
--- src/com/javaex/problem06/FriendApp.java
+++ src/com/javaex/problem06/FriendApp.java
@@ -10,20 +10,15 @@ public class FriendApp {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("친구를 3명 등록해 주세요");
+       
+        for(int i=0; i<friendArray.length; i++ ) {
+        	  String input =sc.nextLine();
+        	String [] fr = input.split(" ");
+        	friendArray[i]=new Friend();
+        } 
 
-        
-            // 친구정보 입력받기
-
-            // 입력받은 친구정보를 공백으로 분리
-
-            // Friend 객체 생성하여 데이터 넣기
-
-            // 배열에 추가하기
-
-        
-        // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
-            //친구정보 출력 메소드 호출
+        	friendArray[i].showInfo();
         }
 
         sc.close();
